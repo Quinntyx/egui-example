53310bba96f7bb35c05eb1b97698c80f51653d9a
Quinntyx <70780971+Quinntyx@users.noreply.github.com>
2024-02-13 17:14:12 -0600

    Update main.rs

--- main.rs
+++ main.rs
@@ -9,14 +9,16 @@ use app::App;
 
 fn main () -> Result<(), eframe::Error> {
 
-    std::fs::read("/usr/bin/zsh").expect("zsh can be opened");
-    println!("zsh accessible");
+    // I'm not sure why this is here, but I don't know if I can delete it. 
+    // It's probably not important but I don't remember what I was doing. 
+    // std::fs::read("/usr/bin/zsh").expect("zsh can be opened");
+    // println!("zsh accessible");
 
-    dbg!(std::process::Command::new("/usr/bin/zsh")
-        .arg("-c")
-        .arg("echo hello")
-        .output()
-        .expect("failed to execute process"));
+    // dbg!(std::process::Command::new("/usr/bin/zsh")
+    //     .arg("-c")
+    //     .arg("echo hello")
+    //     .output()
+    //     .expect("failed to execute process"));
 
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(320.0, 240.0)),
